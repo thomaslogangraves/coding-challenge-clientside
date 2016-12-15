0bc7e6d6c5ee7117f0e77bb19afca71e9e185fa3
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-15 16:10:31 -0500

    change out app file for redux and thunk

---
+++ src/reducers/index.js
@@ -0,0 +1,1 @@
+export { default as referrals } from "./referrals"
--- src/reducers/referrals.js
+++ src/reducers/referrals.js
@@ -1,13 +1,21 @@
+import * as actionsReferrals from "../actions/actionsReferrals"
 
 const initialState = {
-  makeReferralsRequest('https://referly-api.herokuapp.com/')
+  isLoadingReferrals: false,
+  referrals: undefined,
 }
 
-export default (state = initialState, action) => {
-  switch(action.type) {
-    case 'addReferral':
-      return state.push(action.referral)
-    default:
-      return state
+export default function referrals(state=initialState, action={}) {
+  switch (action.type) {
+  case actionsReferrals.FETCH_REFERRALS:
+    return {...state, isLoadingReferrals: true}
+  case actionsReferrals.FETCH_REFERRALS_SUCCESS:
+    return {...state, isLoadingReferrals: false, referrals: action.res}
+  case actionsReferrals.FETCH_REFERRALS_ERROR400:
+  case actionsReferrals.FETCH_REFERRALS_ERROR500:
+  case actionsReferrals.FETCH_REFERRALS_FAILURE:
+    return {...state, isLoadingReferrals: false}
+  default:
+    return state
   }
 }
